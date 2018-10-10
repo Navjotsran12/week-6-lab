aeb1ba9cdb7fdf4d63c2450c5c9feb3c80da2042
Navjotsran12 <43013016+Navjotsran12@users.noreply.github.com>
2018-10-10 14:49:14 -0400

    completed

---
+++ week-6-lab.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=100;
+    cout<<"Enter the value of x";
+    cin>>x;
+     if(x>=86)
+    cout<<"Grade A";
+    else if(x>=72)
+    cout<<"Grade B";
+    else if(x>=60)
+    cout<<"Grade C";
+    else if(x>=50)
+    cout<<"Grade D";
+    else
+    cout<<"Grade F";
+
+   
+
+
+    
+}
